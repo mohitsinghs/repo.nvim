a8a41c50d482891c882f7af5af4459ac75533dc8
Mohit Singh <mohit@mohitsingh.in>
2023-02-20 16:24:11 +0530

    chore: remove linter and add stylua config

--- lua/telescope/_extensions/repo.lua
+++ lua/telescope/_extensions/repo.lua
@@ -13,87 +13,87 @@ local telescope = require("telescope")
 local home_dir = vim.loop.os_homedir()
 
 local function simplify_path(path)
-	if path == Path.path.root() then
-		return path
-	end
+  if path == Path.path.root() then
+    return path
+  end
 
-	if home_dir and vim.startswith(path, home_dir) then
-		return (Path:new("~") / Path:new(path):make_relative(home_dir)).filename
-	end
-	return path
+  if home_dir and vim.startswith(path, home_dir) then
+    return (Path:new("~") / Path:new(path):make_relative(home_dir)).filename
+  end
+  return path
 end
 
 local function path_maker()
-	return function(line)
-		return {
-			value = line,
-			ordinal = line,
-			path = line,
-			display = function(entry)
-				local dir = simplify_path(entry.path)
-				return entry_display.create({
-					items = { {} },
-				})({ dir })
-			end,
-		}
-	end
+  return function(line)
+    return {
+      value = line,
+      ordinal = line,
+      path = line,
+      display = function(entry)
+        local dir = simplify_path(entry.path)
+        return entry_display.create({
+          items = { {} },
+        })({ dir })
+      end,
+    }
+  end
 end
 
 local function find_repos(opts)
-	local repo_command = (function()
-		if opts.repo_command then
-			if type(opts.repo_command) == "function" then
-				return opts.repo_command(opts)
-			end
-			return opts.repo_command
-		elseif vim.fn.executable("repo") == 1 then
-			return { "repo" }
-		end
-	end)()
+  local repo_command = (function()
+    if opts.repo_command then
+      if type(opts.repo_command) == "function" then
+        return opts.repo_command(opts)
+      end
+      return opts.repo_command
+    elseif vim.fn.executable("repo") == 1 then
+      return { "repo" }
+    end
+  end)()
 
-	if not repo_command then
-		utils.notify("repo.find_repos", {
-			msg = "You need to install repo binary",
-			level = "ERROR",
-		})
-		return
-	end
+  if not repo_command then
+    utils.notify("repo.find_repos", {
+      msg = "You need to install repo binary",
+      level = "ERROR",
+    })
+    return
+  end
 
-	local find_repos_args = { "cmp", "--full" }
+  local find_repos_args = { "cmp", "--full" }
 
-	opts.entry_maker = utils.get_lazy_default(opts.entry_maker, path_maker, opts)
+  opts.entry_maker = utils.get_lazy_default(opts.entry_maker, path_maker, opts)
 
-	table.insert(repo_command, find_repos_args)
-	table.insert(repo_command, opts.search_dirs)
-	repo_command = vim.tbl_flatten(repo_command)
+  table.insert(repo_command, find_repos_args)
+  table.insert(repo_command, opts.search_dirs)
+  repo_command = vim.tbl_flatten(repo_command)
 
-	pickers
-		.new({
-			prompt_title = "Repositories",
-			finder = finders.new_oneshot_job(repo_command, opts),
-			previewer = false,
-			sorter = config.file_sorter(opts),
-			layout_strategy = "center",
-			layout_config = {
-				width = 60,
-			},
-			attach_mappings = function(prompt_bufnr)
-				actions_set.select:replace(function()
-					actions.close(prompt_bufnr)
-					local entry = actions_state.get_selected_entry()
-					local dir = from_entry.path(entry)
-					vim.schedule(function()
-						vim.cmd("cd" .. dir)
-					end)
-				end)
-				return true
-			end,
-		})
-		:find()
+  pickers
+    .new({
+      prompt_title = "Repositories",
+      finder = finders.new_oneshot_job(repo_command, opts),
+      previewer = false,
+      sorter = config.file_sorter(opts),
+      layout_strategy = "center",
+      layout_config = {
+        width = 60,
+      },
+      attach_mappings = function(prompt_bufnr)
+        actions_set.select:replace(function()
+          actions.close(prompt_bufnr)
+          local entry = actions_state.get_selected_entry()
+          local dir = from_entry.path(entry)
+          vim.schedule(function()
+            vim.cmd("cd" .. dir)
+          end)
+        end)
+        return true
+      end,
+    })
+    :find()
 end
 
 return telescope.register_extension({
-	exports = {
-		repo = find_repos,
-	},
+  exports = {
+    repo = find_repos,
+  },
 })
